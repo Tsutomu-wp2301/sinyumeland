a2a8aed7bd8fa30a7671a661a7a6bc9598be2205
Tsutomu <tutokun0723@gmail.com>
2023-07-17 23:26:13 +0900

    タブレット版

---
+++ template-parts/navMenu-sp.php
@@ -0,0 +1,16 @@
+<!-- スマホ・タブレット用のグローバルナビテンプレート -->
+
+<div id="gnavi">
+  <ul class="clearfix">
+    <li><a href="<?php echo home_url() ; ?>/tenpo"><img src="<?php echo get_template_directory_uri(); ?>/images/g01.jpg" alt="店舗情報"></a></li>
+    <li><a href="<?php echo home_url() ; ?>/category/media"><img src="<?php echo get_template_directory_uri(); ?>/images/g02.jpg" alt="最新情報"></a></li>
+    <li><a href="<?php echo home_url() ; ?>/p-world"><img src="<?php echo get_template_directory_uri(); ?>/images/g03.jpg" alt="機種情報"></a></li>
+    <li><a href="<?php echo home_url() ; ?>/company"><img src="<?php echo get_template_directory_uri(); ?>/images/g04.jpg" alt="企業情報"></a></li>
+  </ul>
+  <ul class="clearfix">
+    <li><a href="https://www.youtube.com/user/yumelandgroup" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/g05.jpg" alt="動画"></a></li>
+    <li><a href="<?php echo home_url() ; ?>/category/news"><img src="<?php echo get_template_directory_uri(); ?>/images/g06.jpg" alt="夢らんどニュース"></a></li>
+    <li><a href="<?php echo home_url() ; ?>/saiyou"><img src="<?php echo get_template_directory_uri(); ?>/images/g07.jpg" alt="採用情報"></a></li>
+    <li><a href="https://www.facebook.com/taiheiyumeland/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/g08.jpg" alt="Facebook"></a></li>
+  </ul>
+</div>
